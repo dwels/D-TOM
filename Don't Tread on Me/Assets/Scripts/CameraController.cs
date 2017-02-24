598cf4b01822d51867954069636e2496fd5d9686
Paul Lampanaro (RIT Student) <pjl7021@ad.rit.edu>
2017-02-24 11:09:21 -0500

    New camera. Sticks on limits, but leaving it for now. Waypoint creation in progress.

--- Don't Tread on Me/Assets/Scripts/CameraController.cs
+++ Don't Tread on Me/Assets/Scripts/CameraController.cs
@@ -2,31 +2,115 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
 
     // target of camera
     public Transform target;
     public float smoothTime = 0.3f;
     public float xOffset = 10;
     public float zOffset = 10;
+    public int cameraSpeed = 10;
+    public float distanceCap = 100;
+    public Camera camera;
+    public GameObject waypoint;
 
-    private Vector3 velocity = Vector3.zero;
+    private Vector3 oldPosition;
+    private Vector3 oldTargetPosition;
 
     // Use this for initialization
     void Start()
     {
-
+        oldPosition = transform.position;
+        oldTargetPosition = target.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // follow code
-        Vector3 goalPos = target.position;
-        goalPos.x = goalPos.x - xOffset;
-        goalPos.z = goalPos.z - xOffset;
-        goalPos.y = transform.position.y;
-        transform.position = Vector3.SmoothDamp(transform.position, goalPos, ref velocity, smoothTime);
-        transform.LookAt(target);
+        // follow the tank
+        Vector3 follow = target.transform.position - oldTargetPosition;
+        transform.Translate(follow, Space.World);
+
+        // old position
+        oldPosition = transform.position;
+
+        // apple input
+        if (Input.GetKey(KeyCode.I) || Input.GetButton("Y"))
+        {
+            transform.Translate(new Vector3(1, 0, 1) * cameraSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey(KeyCode.K) || Input.GetButton("A"))
+        {
+            transform.Translate(new Vector3(-1, 0, -1) * cameraSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey(KeyCode.J) || Input.GetButton("X"))
+        {
+            transform.Translate(new Vector3(-1, 0, 1) * cameraSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey(KeyCode.L) || Input.GetButton("B"))
+        {
+            transform.Translate(new Vector3(1, 0, -1) * cameraSpeed * Time.deltaTime, Space.World);
+        }
+
+        // check to see if too far away
+        Vector3 a = transform.position;
+        a.y = 0;
+
+        Vector3 b = target.transform.position;
+        b.y = 0;
+        b.x = b.x - xOffset;
+        b.z = b.z - xOffset;
+
+        // because you cant just add staight to the x and z value of transform position
+        Vector3 correction = transform.position;
+
+        // math and stuff
+
+        // Method 1 (rectangular bounds)
+        if (Mathf.Abs((a.x - a.z) - (b.x - b.z)) > distanceCap)
+        {
+            correction.x = oldPosition.x;
+            correction.z = oldPosition.z;
+        }
+
+        if (Mathf.Abs((a.x + a.z) - (b.x + b.z)) > distanceCap)
+        {
+            correction.x = oldPosition.x;
+            correction.z = oldPosition.z;
+        }
+
+        /* Method 2 (circular bounds)
+        if(Vector3.Distance(a, b) > distanceCap)
+        {
+            correction.x = oldPosition.x;
+            correction.z = oldPosition.z;
+        }
+        */
+
+        transform.position = correction;
+
+        // update tank's old position
+        oldTargetPosition = target.transform.position;
+
+        // create waypoints - in progress
+        /*
+        if (Input.GetButton("LT"))
+        {
+            CreateWaypoint();
+        }
+        */
+    }
+
+    // waypoint creator
+    void CreateWaypoint()
+    {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(camera.transform.forward);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Instantiate(waypoint, hit.transform);
+        }
     }
 }
