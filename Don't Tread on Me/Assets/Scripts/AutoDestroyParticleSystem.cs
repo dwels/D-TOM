9c60286edc4366ffe487b43d8995500c1c4f8ef0
Justin John <jxj7701@rit.edu>
2017-04-05 19:05:43 -0400

    EnemyTank, Driver, P System auto destroy
    hooked up projectile to enemyTank so it can shoot again, moved driver
    code out of playerTank for consistency, added script to auto delete
    particle systems

---
+++ Don't Tread on Me/Assets/Scripts/AutoDestroyParticleSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroyParticleSystem : MonoBehaviour {
+
+    private ParticleSystem psys;
+
+	// Use this for initialization
+	void Start () {
+        psys = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (psys)
+        {
+            if (!psys.IsAlive())
+            {
+                Destroy(gameObject);
+            }
+        }
+	}
+}
